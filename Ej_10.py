180bc4242c1dcc39e0ff0bb0eda88ea0c108db45
CeciR-Tybot <68873734+CeciR-Tybot@users.noreply.github.com>
2022-08-13 10:02:23 -0300

    Add files via upload

---
+++ Ej_10.py
@@ -0,0 +1,6 @@
+#Crear una lista denominada “Clientes” con los nombres de los diferentes  dueños de perros.
+#Ordenarla alfabéticamente y mostrarla por pantalla.
+
+CLIENTES = ["Juan", "Mario", "Ariel", "Josefina", "Marianella"]
+
+print(sorted(CLIENTES))
