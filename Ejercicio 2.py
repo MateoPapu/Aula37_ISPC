edaa71220b7e94527d979071fae8f565d001b990
MateoPapu <92344355+MateoPapu@users.noreply.github.com>
2022-08-12 23:40:45 -0300

    Add files via upload

---
+++ Ejercicio 2.py
@@ -0,0 +1,11 @@
+Dueno2 = [23546987, "Maria", "Perez", 4789689, "Pueyrredon  811"]
+
+for recorrer in Dueno2:
+    if recorrer == 23546987:
+        continue
+    elif recorrer == 4789689:
+        continue
+    else:
+        print(recorrer)
+
+
